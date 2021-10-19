249610088fd38d416afd39da4f2d7931ce244f8c
andreywastaken <andreihombak@gmail.com>
2021-10-19 17:47:39 +0300

    lab5 fixed

--- мча/lab5.py
+++ мча/lab5.py
@@ -1,10 +1,11 @@
 import matplotlib.pyplot as plt
 import math
-from sympy import Symbol, solve, parse_expr, lambdify
+from sympy import parse_expr, lambdify
 
 
 """
 Вариант 24
+u'' + p * u' + q * u = f
 u'' - 2 * u' + 1 * u = 2e^x / (x + 1) ^ 3
 u(0) = 1
 u'(1) = e / 4
@@ -24,26 +25,55 @@ def task(a=0, b=1, alpha0=1, alpha1=0, beta0=0, beta1=1, A=1, B=math.e / 4,
     x = [a + h * i for i in range(n + 1)]
 
     lams = {v: lambdify(list(v.free_symbols), v) for v in [p, q, f, u]}
-    p = [lams[p](x[i]) if p.free_symbols else lams[p]() for i in range(n + 1)]
-    q = [lams[q](x[i]) if q.free_symbols else lams[q]() for i in range(n + 1)]
-    f = [lams[f](x[i]) if f.free_symbols else lams[f]() for i in range(n + 1)]
-    u = [lams[u](x[i]) if u.free_symbols else lams[u]() for i in range(n + 1)]
+    p, q, f, u = map(lambda func: get_values(x, lams, func), (p, q, f, u))
 
-    system, yi = [], [Symbol('y0'), Symbol('y1')]
+    matrix = make_tridiagonal_matrix(p, q, f, n, A, B, alpha0, alpha1, beta0, beta1, h)
 
-    for i in range(n - 1):
-        yi.append(Symbol(f'y{i + 2}'))
-        system.append(
-            (2 - h * p[i]) * yi[i] + (2 * h * h * q[i] - 4) * yi[i + 1] + (2 + h * p[i]) * yi[i + 2] - 2 * h * h * f[i]
-        )
-    system.append(alpha0 * yi[0] + alpha1 * (yi[1] - yi[0]) / h - A)
-    system.append(beta0 * yi[n] + beta1 * (yi[n] - yi[n - 1]) / h - B)
+    y = tridiagonal_matrix_algo(*matrix)
 
-    y = solve(system, *yi).values()
     plt.plot(x, y, label='приближенное решение')
     plt.plot(x, u, label='точное решение')
     plt.legend()
     plt.show()
+
+
+def tridiagonal_matrix_algo(a_, b_, c_, d_):
+    a, b, c, d = map(lambda x: x.copy(), (a_, b_, c_, d_))
+    n = len(d)
+    for i in range(1, n):
+        m = a[i - 1] / b[i - 1]
+        b[i] = b[i] - m * c[i - 1]
+        d[i] = d[i] - m * d[i - 1]
+
+    y = b
+    y[-1] = d[-1] / b[-1]
+
+    for i in range(n - 2, -1, -1):
+        y[i] = (d[i] - c[i] * y[i + 1]) / b[i]
+
+    return y
+
+
+def get_values(x, lams, f):
+    return [lams[f](x[i]) if f.free_symbols else lams[f]() for i in range(len(x))]
+
+
+def make_tridiagonal_matrix(p, q, f, n, A, B, alpha0, alpha1, beta0, beta1, h):
+    # y0 *(h * alpha0 - alpha1) + alpha1 * y1 = A * h
+    a, b, c, d = [], [h * alpha0 - alpha1], [alpha1], [A * h]
+
+    for i in range(n - 1):
+        a.append((2 - h * p[i]))
+        b.append(2 * h * h * q[i] - 4)
+        c.append(2 + h * p[i])
+        d.append(2 * h * h * f[i])
+
+    # yn * (h * beta0 + beta1) - beta1 * yn-1 = B * h
+    a.append(-beta1)
+    b.append(h * beta0 + beta1)
+    d.append(B * h)
+
+    return a, b, c, d
 
 
 if __name__ == '__main__':
